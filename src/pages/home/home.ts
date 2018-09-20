995b6fff2ac83f1491d6147bd54168c62500d2e7
Mauricio Grejanin <mgrejanin@eurekaria.com>
2018-09-19 23:04:07 -0300

    ta indo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,6 +5,12 @@ import { TextToSpeech } from '@ionic-native/text-to-speech';
 
 declare var ApiAIPromises: any;
 
+class Chat {
+  type: string;
+  message: string;
+  createdAt: Date;
+}
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
@@ -12,9 +18,9 @@ declare var ApiAIPromises: any;
 })
 export class HomePage {
 
-  question: string = '';
-  answers = [];
+  chats: Array<Chat> = [];
   listening = false;
+  message: string;
 
   constructor(
     public navCtrl: NavController,
@@ -24,9 +30,8 @@ export class HomePage {
     private tts: TextToSpeech) {
     platform.ready().then(() => {
       ApiAIPromises.init({
-        clientAccessToken: "769af9a04dac4a8495ef3802c14ccd30"
-      }).then(res => {
-        console.log("res:", res);
+        clientAccessToken: "769af9a04dac4a8495ef3802c14ccd30",
+        lang:'pt-BR'
       })
 
       this.speechRecognition.hasPermission()
@@ -40,28 +45,96 @@ export class HomePage {
               )
           }
         })
+
+      ApiAIPromises.setListeningStartCallback(function () {
+        console.log("listening started");
+      });
+
+      ApiAIPromises.setListeningFinishCallback(function () {
+        console.log("listening stopped");
+      });
     })
+
+
   }
 
-  ask() {
+  sendMessage() {
+    this.addChat(this.message, 'user');
+    ApiAIPromises.requestText({
+      query: this.message
+    })
+      .then(({ result: { fulfillment: { speech } } }) => {
+        this.ngZone.run(() => {
+          this.tts.speak({ text: speech, locale: 'pt-BR', rate: 1.6 })
+            .then(() => console.log('Success'))
+            .catch((reason: any) => console.log(reason));
+          this.addChat(speech, 'bot');
+        });
+      })
+  }
+
+  async sendText() {
+    try {
+    this.addChat(this.message, 'user');
+    this.addChat((await ApiAIPromises.requestText({query: this.message})).result.fulfillment.speech, 'bot')
+    } catch (e) {
+      console.log('errorere:', e);
+    }
+  }
+
+  async sendVoice() {
+    ApiAIPromises.requestVoice({query: this.message}
+      )
+      .then(function (response) {
+          // some response processing
+          console.log('success', response);
+      })
+      .fail(function (error) {
+          // some error processing
+          console.log(error);
+      });
+    // try {
+    //   // this.addChat(this.message, 'user');
+    //   console.log(await ApiAIPromises.requestVoice());
+    //   // this.addChat((await ApiAIPromises.requestVoice({query: this.message})).result.fulfillment.speech, 'bot')
+    //   } catch (e) {
+    //     console.log('errorere:', e);
+    //   }
+    // try {
+    // this.addChat(this.message, 'user');
+
+    //   ApiAIPromises.levelMeterCallback(function (level) {
+    //     console.log(level);
+    //   });
+
+    //   ApiAIPromises.requestVoice(
+    //     {}, // empty for simple requests, some optional parameters can be here
+    //     function (response) {
+    //       console.log(response)
+    //       // place your result processing here
+    //       alert(JSON.stringify(response));
+    //     },
+    //     function (error) {
+    //       // place your error processing here
+    //       alert(error);
+    //     });
+    // } catch (e) {
+    //   alert(e);
+    // }
+  }
+
+  addChat(message: string, type: string) {
+    this.chats.push({ message: message, type: type, createdAt: new Date() });
+  }
+
+  startListen() {
     if (!this.listening) {
       this.listening = true;
       this.speechRecognition.startListening({ language: 'pt-BR' })
         .subscribe(
           (matches: Array<string>) => {
-            console.log(matches);
-            this.question = matches[0];
-            ApiAIPromises.requestText({
-              query: matches[0]
-            })
-              .then(({ result: { fulfillment: { speech } } }) => {
-                this.ngZone.run(() => {
-                  this.tts.speak({text:speech, locale: 'pt-BR', rate: 1.5})
-                    .then(() => console.log('Success'))
-                    .catch((reason: any) => console.log(reason));
-                  this.answers.push(speech);
-                });
-              })
+            alert(matches);
+            this.sendMessage();
           },
           (onerror) => console.log('error:', onerror)
         )
@@ -69,6 +142,10 @@ export class HomePage {
       this.listening = false;
       this.speechRecognition.stopListening();
     }
+  }
+
+  cancelar(){
+    ApiAIPromises.cancelAllRequests();
   }
 
 }
